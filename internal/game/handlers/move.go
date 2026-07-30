ea8147cfc4114d6679358fd8535aeb60ceed0533
1tzArad <147817491+1tzArad@users.noreply.github.com>
2026-07-30 13:15:23 +0330

    Refactor error messages in chat and move handlers for clarity

--- internal/game/handlers/move.go
+++ internal/game/handlers/move.go
@@ -9,12 +9,12 @@ func moveHandler(world *game.World) network.HandlerFunc {
 	return func(c *network.Context) {
 		var data game.MovePayload
 		if err := c.Bind(&data); err != nil {
-			c.ReplyError("invalid_body", "invalid body!")
+			c.ReplyError("invalid_body", "request body is invald!")
 			return
 		}
 
 		if !data.Direction.IsValid() {
-			c.ReplyError("invalid_move", "invalid move!")
+			c.ReplyError("invalid_move", "move direction is not valid!")
 			return
 		}
 
