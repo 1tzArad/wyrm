4f7c1ed8ac3b2f7a36cbc0c43c783def5c71156b
1tzArad <147817491+1tzArad@users.noreply.github.com>
2026-08-01 13:15:05 +0330

    Update PlayerStore interface to return pointers for player retrieval and creation methods

--- internal/game/store.go
+++ internal/game/store.go
@@ -11,7 +11,7 @@ import (
 type PlayerStore interface {
 	SavePlayer(ctx context.Context, p *player.Player) error
 	LoadPlayer(ctx context.Context, id uuid.UUID) (*player.Player, error)
-	GetByID(ctx context.Context, id uuid.UUID) (sqlc.Player, error)
-	GetByUserID(ctx context.Context, userID uuid.UUID) (sqlc.Player, error)
-	Create(ctx context.Context, arg sqlc.CreatePlayerParams) (sqlc.Player, error)
+	GetByID(ctx context.Context, id uuid.UUID) (*player.Player, error)
+	GetByUserID(ctx context.Context, userID uuid.UUID) (*player.Player, error)
+	Create(ctx context.Context, arg sqlc.CreatePlayerParams) (*player.Player, error)
 }
