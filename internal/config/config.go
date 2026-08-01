2627ee62ea4c50af1d8f43b3e0a50f68a6d2e333
1tzArad <147817491+1tzArad@users.noreply.github.com>
2026-08-01 13:27:47 +0330

    Refactor config setup to include Postgres configuration and error handling for port conversion

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,39 @@
 package config
 
+import (
+	"strconv"
+
+	"github.com/charmbracelet/log"
+)
+
 type Config struct {
-	JWTSecret string
+	JWTSecret  string
+	PostgresDB *PostgresConfig
+}
+
+type PostgresConfig struct {
+	User string
+	Pass string
+	Host string
+	Port int
+	Name string
 }
 
 func Setup() *Config {
+	postgres_db_port_str := GetEnv("POSTGRES_PORT")
+	postgres_db_port, err := strconv.Atoi(postgres_db_port_str)
+	if err != nil {
+		log.Fatal("POSTGRES_PORT must be a number!")
+		return nil
+	}
 	return &Config{
 		JWTSecret: GetEnv("JWT_SECRET"),
+		PostgresDB: &PostgresConfig{
+			User: GetEnv("POSTGRES_USER"),
+			Pass: GetEnv("POSTGRES_PASS"),
+			Host: GetEnv("POSTGRES_HOST"),
+			Port: postgres_db_port,
+			Name: GetEnv("POSTGRES_NAME"),
+		},
 	}
 }
